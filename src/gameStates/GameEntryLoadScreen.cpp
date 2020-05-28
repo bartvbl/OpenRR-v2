38911dc1118580265de0c01aea076780815d692b
bartvbl <bartvblokl@gmail.com>
2020-05-29 01:58:48 +0200

    Added a number of resource files

--- src/gameStates/GameEntryLoadScreen.cpp
+++ src/gameStates/GameEntryLoadScreen.cpp
@@ -1,6 +1,7 @@
 #include "GameEntryLoadScreen.h"
 
 void GameEntryLoadScreen::init(ore::resources::ResourceCache *cache) {
+    ore::resources::MeshResource* chrystal = cache->meshes.getResource_Blocking("chrystal_loadScreen");
 
 }
 
--- src/gameStates/GameEntryLoadScreen.h
+++ src/gameStates/GameEntryLoadScreen.h
@@ -1,9 +1,10 @@
 #pragma once
 
 
-#include <ore/resouces/loadScreen/LoadScreenRenderer.h>
+#include <ore/resources/loadScreen/LoadScreenRenderer.h>
 
 class GameEntryLoadScreen : public ore::resources::LoadScreenRenderer {
+public:
     void init(ore::resources::ResourceCache *cache) override;
     void draw(float progress) override;
 };
